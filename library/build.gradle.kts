c64a2e4df60757adb09f06ae555741f2dc08f42f
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-09-09 09:25:52 +0200

    Link commit!

---
+++ library/build.gradle.kts
@@ -0,0 +1,136 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "com.tecknobit.kassaforte"
+version = "1.0.0"
+
+kotlin {
+    jvm()
+    androidTarget {
+        publishLibraryVariants("release")
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_18)
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+        macosX64(),
+        macosArm64()
+    ).forEach { appleTarget ->
+        appleTarget.binaries.framework {
+            baseName = "kassaforte"
+            isStatic = true
+        }
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        binaries.executable()
+        browser {
+            webpackTask {
+                dependencies {
+                }
+            }
+        }
+    }
+
+    sourceSets {
+
+        val androidMain by getting {
+            dependencies {
+
+            }
+        }
+
+        val commonMain by getting {
+            dependencies {
+            }
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val macosX64Main by getting
+        val macosArm64Main by getting
+        val appleMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            macosX64Main.dependsOn(this)
+            macosArm64Main.dependsOn(this)
+            dependencies {
+
+            }
+        }
+
+        val wasmJsMain by getting {
+            dependencies {
+            }
+        }
+
+    }
+}
+
+/*
+mavenPublishing {
+    configure(
+        platform = KotlinMultiplatform(
+            javadocJar = JavadocJar.Dokka("dokkaHtml"),
+            sourcesJar = true
+        )
+    )
+    coordinates(
+        groupId = "io.github.n7ghtm4r3",
+        artifactId = "equinox-navigation",
+        version = "1.0.3"
+    )
+    pom {
+        name.set("Equinox Navigation")
+        description.set("Utilities to handle the navigation in Compose Multiplatform applications")
+        inceptionYear.set("2025")
+        url.set("https://github.com/N7ghtm4r3/Equinox")
+
+        licenses {
+            license {
+                name.set("APACHE2")
+                url.set("https://opensource.org/license/apache-2-0")
+            }
+        }
+        developers {
+            developer {
+                id.set("N7ghtm4r3")
+                name.set("Manuel Maurizio")
+                email.set("maurizio.manuel2003@gmail.com")
+                url.set("https://github.com/N7ghtm4r3")
+            }
+        }
+        scm {
+            url.set("https://github.com/N7ghtm4r3/Equinox")
+        }
+    }
+    publishToMavenCentral()
+    signAllPublications()
+}
+ */
+
+android {
+    namespace = "com.tecknobit.kassaforte"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
+    }
+}
