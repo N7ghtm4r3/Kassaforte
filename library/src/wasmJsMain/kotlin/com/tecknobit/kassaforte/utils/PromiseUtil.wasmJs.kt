d930b1ee7ab6ee6d0c0d9f8fdf045c67b6821ea2
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-02-03 17:20:22 +0100

    Documented the await method in PromiseUtil and completed the update

--- library/src/wasmJsMain/kotlin/com/tecknobit/kassaforte/utils/PromiseUtil.wasmJs.kt
+++ library/src/wasmJsMain/kotlin/com/tecknobit/kassaforte/utils/PromiseUtil.wasmJs.kt
@@ -5,6 +5,13 @@ package com.tecknobit.kassaforte.utils
 import kotlinx.coroutines.await
 import kotlin.js.Promise
 
+/**
+ * Wrapper method to use await correctly a [Promise]
+ *
+ * @return the result of the promise as [T]
+ *
+ * @param T the type the promise have to return when resolved
+ */
 actual suspend fun <T : JsAny?> Promise<T>.await(): T {
     return this.await()
 }
--- library/src/webMain/kotlin/com/tecknobit/kassaforte/utils/PromiseUtil.kt
+++ library/src/webMain/kotlin/com/tecknobit/kassaforte/utils/PromiseUtil.kt
@@ -6,4 +6,11 @@ import kotlin.js.ExperimentalWasmJsInterop
 import kotlin.js.JsAny
 import kotlin.js.Promise
 
+/**
+ * Wrapper method to use await correctly a [Promise]
+ *
+ * @return the result of the promise as [T]
+ *
+ * @param T the type the promise have to return when resolved
+ */
 expect suspend fun <T : JsAny?> Promise<T>.await(): T
