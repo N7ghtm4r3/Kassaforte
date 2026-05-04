cb31e137831178847395e65217c1a657b294bc29
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-05-04 09:17:49 +0200

    docu(EcdhKeyDeriveParams) documented the interface

--- library/src/webMain/kotlin/com/tecknobit/kassaforte/wrappers/crypto/key/genspec/EcdhKeyDeriveParams.kt
+++ library/src/webMain/kotlin/com/tecknobit/kassaforte/wrappers/crypto/key/genspec/EcdhKeyDeriveParams.kt
@@ -2,9 +2,22 @@ package com.tecknobit.kassaforte.wrappers.crypto.key.genspec
 
 import com.tecknobit.kassaforte.wrappers.crypto.key.CryptoKey
 
-// TODO: TO DOCU
+/**
+ * The `EcdhKeyDeriveParams` interface wraps the native [EcdhKeyDeriveParams](https://developer.mozilla.org/en-US/docs/Web/API/EcdhKeyDeriveParams)
+ * object
+ *
+ * @author Tecknobit - N7ghtm4r3
+ *
+ * @see kotlin.js.JsAny
+ * @see com.tecknobit.kassaforte.wrappers.crypto.key.genspec.KeyGenSpec
+ *
+ * @since Revision Threee
+ */
 external interface EcdhKeyDeriveParams : KeyGenSpec {
 
+    /**
+     * `public` The object representing the public key of the other entity
+     */
     val public: CryptoKey
 
 }
